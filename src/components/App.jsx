237ba213c6acf4ed59a40cbf87a0afbd610630ea
Ethan Cornwill <77322111+finneh4249@users.noreply.github.com>
2025-02-01 17:00:15 +1100

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -24,17 +24,6 @@ function App() {
           {/* Add more routes here */}
         </Routes>
         <Footer />
-<script type="text/javascript">
-var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-(function(){
-var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-s1.async=true;
-s1.src='https://embed.tawk.to/679da9ec3a84273260786e64/1iivtvhh8';
-s1.charset='UTF-8';
-s1.setAttribute('crossorigin','*');
-s0.parentNode.insertBefore(s1,s0);
-})();
-</script>
 
       </div>
     </BrowserRouter>
